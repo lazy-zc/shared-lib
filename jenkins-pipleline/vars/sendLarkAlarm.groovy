f1a4f72468148ae383f496f708bdaab3e55ed0ee
lazy-zc <lazyboyzc@gmail.com>
2025-01-24 12:28:10 +0800

    Update sendLarkAlarm.groovy

--- jenkins-pipleline/vars/sendLarkAlarm.groovy
+++ jenkins-pipleline/vars/sendLarkAlarm.groovy
@@ -2,10 +2,27 @@ def call(String apiUrl, String jobName, String jobStatus) {
     
     def payload = """
     {
-        "msg_type": "text",
-        "content": {
-            "text": "Jenkins job '${jobName}' has failed with status: ${jobStatus}. Please investigate."
-        }
+      "msg_type": "interactive",
+      "card": {
+        "config": {
+          "wide_screen_mode": true
+        },
+        "header": {
+          "title": {
+            "tag": "plain_text",
+            "content": "⚠️ Jenkins Job Alert"
+          }
+        },
+        "elements": [
+          {
+            "tag": "div",
+            "text": {
+              "tag": "lark_md",
+              "content": "Jenkins job '${jobName}' : ${jobStatus}."
+            }
+          }
+        ]
+      }
     }
     """
     
@@ -13,7 +30,7 @@ def call(String apiUrl, String jobName, String jobStatus) {
         def response = httpRequest(
             url: apiUrl,
             httpMode: 'POST',
-            contentType: 'APPLICATION_JSON',
+            contentType: 'APPLICATION_JSON_UTF8',
             requestBody: payload,
             quiet: true
         )
